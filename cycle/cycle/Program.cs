963e18a9e25a54add230c0dbaeeaed70eae0c8eb
AvanMyers <smail1341@gmail.com>
2022-08-10 17:54:06 +0300

    cycle

---
+++ cycle/cycle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string massage;
+            int numberOfRepit;
+
+            Console.Write("Введите сообщение: ");
+            massage = Console.ReadLine();
+            Console.Write("Введите количество повторений: ");
+            numberOfRepit = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < numberOfRepit; i++)
+            {
+                Console.WriteLine($"{i + 1}. {massage}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
